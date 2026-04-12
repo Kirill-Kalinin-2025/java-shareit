eeedd040921a9ed8e37f77faac29afe140b74f14
Kirill-Kalinin-2025 <kirya.kalina.06@yandex.ru>
2026-04-12 16:19:20 +0300

    Add bookings. Part 3.

--- src/main/java/ru/practicum/shareit/item/storage/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/storage/CommentRepository.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.item.storage;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.item.model.Comment;
